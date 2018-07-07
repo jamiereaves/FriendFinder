009bec3084725ff36391541b9f9dd52c2032b967
jamie reaves <jamiereaves@utexas.edu>
2018-07-07 05:19:49 -0500

    commented out the apiRoutes.js file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,16 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+// linking the route to a "data" source.
+// This data source holds an array of information on friends.js
+
 
 var friendData = require("../data/friends");
+//set up global variables to hold information used to determine the best match
 var compatibilityIndex = 0;
 var compatibilityArray = [];
 var smallestIndexValue = 0;
 var smallestIndex = 0;
-
+//function that determines the index (of friendData) that corresponds to the the smallest (i.e. most compatible) compatibility index
 var mostCompatibleFinder = function(){
 
     smallestIndexValue = compatibilityArray[0];
@@ -25,17 +26,15 @@ var mostCompatibleFinder = function(){
             }
         }
     }
-    console.log("the smallest index is: " + smallestIndex + "it's value is: " + smallestIndexValue);
 };
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // API GET Request
+  // when the user visits the link
+  // (ex: localhost:PORT/api/friends => a JSON display of the data in the friendData)
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
@@ -43,51 +42,35 @@ module.exports = function(app) {
   });
 
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
+  // API POST Request
+  // when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    console.log(req.body.name);    
-    console.log(req.body.scores);
+        //for loop within a for loop that determines the compatibility index (i.e. the total of the absolute value
+        //of the difference between each corresponding index in the existing users' scores array and in the new user's scores array)
         for (h = 0 ; h < friendData.length; h++) {
-            console.log(friendData[h].name)
-            console.log(friendData[h].scores);
             for (i = 0; i<friendData[h].scores.length ; i++){
                 compatibilityIndex += Math.abs(friendData[h].scores[i] - parseInt(req.body.scores[i]));
             }
-            console.log(req.body.name + " / " + friendData[h].name + " compatibility = " + compatibilityIndex);
+            //push each compatibilityIndex value to the compatibility Array
             compatibilityArray.push(compatibilityIndex);
-            console.log(compatibilityArray);
             //clear compatibility index
             compatibilityIndex = 0;
         }
+        //call the mostCompatibleFinder function to determine the index of the user of friendData with the smallest compatibility index
+        //I know that this will always return the last user in  the case of a tie...I hope to add a function that will deal with displaying
+        //all users in the case of a tie in compatibility
         mostCompatibleFinder();
+        //empty compatibilityArray for future entries
         compatibilityArray = [];
+        //push the new user's object to friendData
         friendData.push(req.body);
-        console.log(friendData[smallestIndex]);
+        //send back the most compatible user's object data
         res.json(friendData[smallestIndex]);
 
-      //write a loop to check compatibility
     
   });
 
-  /* ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
-  });*/
+  
 };
